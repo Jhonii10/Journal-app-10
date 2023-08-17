ae0193296990718d4cc345c994cab2f49d0d0201
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-17 12:57:32 -0500

    pwa en error en production 7

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,37 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
+    import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'src/assets/*'], // Añade los activos necesarios aquí
-      manifest: {
-        name: 'Journal App',
-        short_name: 'Mi PWA',
-        description: 'Una increíble app de notas.',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: '/src/assets/vite.svg', // Ruta relativa al directorio público
-            sizes: '192x192',
-            type: 'image/png',
+    export default defineConfig({
+      plugins: [
+        react(),
+        VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: {
+            enabled: true
           },
-          {
-            src: '/src/assets/vite.svg', // Ruta relativa al directorio público
-            sizes: '512x512',
-            type: 'image/png',
+          workbox: {
+            globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+        },
+          manifest: {
+            name: 'Journal App',
+            short_name: 'Mi PWA',
+            description: 'Una increíble app de notas.',
+            theme_color: '#ffffff', // Color de la barra de navegación
+            background_color: '#ffffff', // Color de fondo
+            icons: [
+              {
+                src: 'src/assets/vite.svg', // Ruta absoluta al ícono de la PWA
+                sizes: '192x192',
+                
+              },
+              {
+                src: 'src/assets/vite.svg', // Ruta absoluta al ícono de la PWA
+                sizes: '512x512',
+                
+              },
+            ],
           },
-        ],
-      },
-    }),
-  ],
-});
-
+        }),
+      ],
+    })
