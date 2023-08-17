2005e73fd4412077feeccd97f289907d6015cf66
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-17 11:16:05 -0500

    pwa en aplicacion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Journal App',
+        short_name: 'Mi PWA',
+        description: 'Una increíble app de notas.',
+        theme_color: '#ffffff', // Color de la barra de navegación
+        background_color: '#ffffff', // Color de fondo
+        icons: [
+          {
+            src: './src/assets/vite.svg', // Ruta al ícono de la PWA
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './src/assets/vite.svg', // Ruta al ícono de la PWA
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
