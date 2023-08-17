220fb2586876542c2e103f10b70bc80e57862f01
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-17 14:47:10 -0500

    pwa en error en production 8

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,16 @@
-    import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-    import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import {fileURLToPath} from 'node:url'
 
-    export default defineConfig({
-      plugins: [
-        react(),
-        VitePWA({
-          registerType: 'autoUpdate',
-          devOptions: {
-            enabled: true
-          },
-          workbox: {
-            globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
-        },
-          manifest: {
-            name: 'Journal App',
-            short_name: 'Mi PWA',
-            description: 'Una increíble app de notas.',
-            theme_color: '#ffffff', // Color de la barra de navegación
-            background_color: '#ffffff', // Color de fondo
-            icons: [
-              {
-                src: 'src/assets/vite.svg', // Ruta absoluta al ícono de la PWA
-                sizes: '192x192',
-                
-              },
-              {
-                src: 'src/assets/vite.svg', // Ruta absoluta al ícono de la PWA
-                sizes: '512x512',
-                
-              },
-            ],
-          },
-        }),
-      ],
-    })
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
+  resolve:{
+    alias:{
+      '@':fileURLToPath(new URL('./src',import.meta.url))
+    }
+  }
+});
