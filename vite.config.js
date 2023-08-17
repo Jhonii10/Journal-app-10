b98b3cd4b7366693fefda3555d26b2a3e8eb3a3f
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-17 12:44:55 -0500

    pwa en error en production 6

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
-     },
+      includeAssets: ['favicon.ico', 'robots.txt', 'src/assets/*'], // Añade los activos necesarios aquí
       manifest: {
         name: 'Journal App',
         short_name: 'Mi PWA',
         description: 'Una increíble app de notas.',
-        theme_color: '#ffffff', // Color de la barra de navegación
-        background_color: '#ffffff', // Color de fondo
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
         icons: [
           {
-            src: 'src/assets/vite.svg', // Ruta absoluta al ícono de la PWA
+            src: '/src/assets/vite.svg', // Ruta relativa al directorio público
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'src/assets/vite.svg', // Ruta absoluta al ícono de la PWA
+            src: '/src/assets/vite.svg', // Ruta relativa al directorio público
             sizes: '512x512',
             type: 'image/png',
           },
@@ -34,4 +29,5 @@ export default defineConfig({
       },
     }),
   ],
-})
+});
+
